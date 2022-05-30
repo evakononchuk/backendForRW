0f79e1df49624bdf9227141a61a30a00ad23d5bf
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-05-30 20:27:57 +0300

    Take a name from query parameter and return "Hello ${name}" or "Hello world"

--- server.js
+++ server.js
@@ -3,7 +3,34 @@
 const fastify = require('fastify')({ logger: true });
 
 // Declare a route
-fastify.get('/', async (request, reply) => ({ Hello: 'World' }));
+//fastify.get('/', async (request, reply) => ({ Hello: 'World' }));
+
+fastify.route({
+  method: 'GET',
+  url: '/',
+  schema: {
+    // request needs to have a querystring with a `name` parameter
+    querystring: {
+      name: { type: 'string' },
+    },
+   // the response needs to be an object with `hello`
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' },
+        },
+      },
+    },
+  },
+  // this function is executed for every request before the handler is executed
+  preHandler: async (request, reply) => {
+    // E.g. check authentication
+  },
+  handler: async (request, reply) => {
+    return { hello: 'world' };
+  },
+});
 
 // Run the server!
 const start = async () => {

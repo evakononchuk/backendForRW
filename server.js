47e13768f8058dbb3b47fc9229e09da9727d6938
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-05-31 18:32:10 +0300

    corrected server.js

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
 'use strict';
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify');
 
-// Declare a route
-//fastify.get('/', async (request, reply) => ({ Hello: 'World' }));
-
-fastify.route({
-  method: 'GET',
-  url: '/',
-  schema: {
-    // request needs to have a querystring with a `name` parameter
-    querystring: {
-      name: { type: 'string' },
-    },
-   // the response needs to be an object with `hello`
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: { type: 'string' },
-        },
-      },
-    },
-  },
-  // this function is executed for every request before the handler is executed
-  preHandler: async (request, reply) => {
-    // E.g. check authentication
-  },
-  handler: async (request, reply) => {
-    return { hello: 'world' };
-  },
+fastify.get('/', async (request, reply) => {
+  const name = location.search;
+  reply.send(name ? console.log('Hello ${name}') : console.log('Hello world'));
 });
 
-// Run the server!
 const start = async () => {
   try {
     await fastify.listen(3000);

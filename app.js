f9408f41986397da93487ba82fc07ecd3e48209c
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-06-09 12:11:15 +0300

    Working with tests

--- app.js
+++ app.js
@@ -3,12 +3,9 @@
 const fastify = require('fastify');
 const app = fastify();
 
-function data() {
-  app.get('/', async (request, reply) => {
-    const name = request.query.name;
-    reply.send(name ? `Hello ${name}` : 'Hello world');
-  });
-  return app;
-}
+app.get('/', async (request, reply) => {
+  const name = request.query.name;
+  reply.send(name ? `Hello ${name}` : 'Hello world');
+});
 
-module.exports = data;
+module.exports = app;

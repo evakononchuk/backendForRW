3477c428fb12e88c2dd8a1f6ee440a8e11897e17
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-06-12 18:37:28 +0300

    fix server after rebase

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-"use strict";
-const fastify = require("fastify")();
-
-fastify.get("/", async (request, reply) => {
-  const name = request.query.name;
-  reply.send(name ? `Hello ${name}` : "Hello world");
-});
-=======
 'use strict';
+const app = require('./app');
 
-const server = require('./app');
->>>>>>> 1a44738 (- server.js was divided into files:.js app and server.js)
-
-server.listen(3000, err, address) => {
+app.listen({ port: 3000 }, err => {
   if (err) {
-    server.log.error(err);
+    app.log.error(err);
     process.exit(1);
-  };
-};
+  }
+});

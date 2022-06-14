f4d9a535b03b8bb7eff0071d0b2516e403f54e32
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-06-14 12:53:25 +0300

    Merge 2 test files in hello.test.js

--- hello.test.js
+++ hello.test.js
@@ -2,12 +2,22 @@
 
 const app = require('./app');
 
-test('When query parameter is not provided returns "Hello world"', async () => {
-  const response = await app.inject({
-    method: 'GET',
-    url: '/',
+describe('Using query parameters', () => {
+  test('Without query parameters - returns "Hello world"', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/',
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.query).toBe('Hello world');
   });
-
-  expect(response.statusCode).toBe(200);
-  expect(response.query).toBe('Hello world');
+  test('Gets name from query parameters, returns "Hello ${name}"', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      query: {
+        name: 'Ted',
+      },
+    });
+    expect(response.query).toBe('Hello Ted');
+  });
 });
